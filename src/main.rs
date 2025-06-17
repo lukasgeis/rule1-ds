e72bb67fd0a9090485bdbaffc2d3cff1f4357bd7
Lukas Geis <lukas.geis@ae.cs.uni-frankfurt.de>
2025-06-17 14:43:03 +0200

    Individual Impl

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,11 @@ mod graph;
 mod io;
 mod rule1;
 
-use smallvec::SmallVec;
-use stream_bitset::prelude::{BitmaskStreamConsumer, ToBitmaskStream};
 use structopt::StructOpt;
 
 use crate::{domset::DominatingSet, graph::*, rule1::Rule1};
 
-use std::{collections::HashMap, io::Write};
+use std::{io::Write, time::Instant};
 
 #[derive(StructOpt)]
 struct Args {
@@ -25,141 +23,83 @@ fn main() -> std::io::Result<()> {
     let mut stderr = std::io::stderr();
 
     // Read graph
-    let mut graph = Graph::try_read_pace(std::io::stdin().lock())?;
-    let mut domset = DominatingSet::new();
-
-    // Clone graph for later
-    let org_graph = graph.clone();
+    let org_graph = Graph::try_read_pace(std::io::stdin().lock())?;
 
     writeln!(
         stderr,
         "Graph loaded n={:7} m={:8}",
-        graph.len(),
-        graph.num_edges()
+        org_graph.len(),
+        org_graph.num_edges()
     )?;
 
-    // Run Rule
-    Rule1::apply_rule(&graph, &mut domset);
+    // Run Rules
 
-    // Reduce graph in several steps --- this is inefficient as we want to log multiple levels of reduction
-    let mut markers: Vec<SmallVec<[Node; 2]>> = vec![Default::default(); graph.len() as usize];
-    for u in domset.iter() {
-        for v in graph.closed_neighbors_of(u) {
-            markers[v as usize].push(u);
-        }
-    }
-
-    let mut naive_nodes = BitSet::new(graph.len());
-    let mut linear_nodes = BitSet::new(graph.len());
-
-    let mut node_markers: HashMap<Node, Node> = HashMap::new();
-    'outer: for u in graph.vertices() {
-        // Ignore DomSet-Nodes itself for deletion
-        if markers[u as usize].is_empty() || markers[u as usize].contains(&u) {
-            continue;
-        }
-
-        node_markers.clear();
-        for &m in &markers[u as usize] {
-            node_markers.insert(m, 0);
-        }
-
-        for v in graph.neighbors_of(u) {
-            if markers[v as usize].is_empty() {
-                continue 'outer;
-            }
-
-            for &m in &markers[v as usize] {
-                node_markers.entry(m).and_modify(|num| *num += 1);
-            }
-        }
-
-        for &m in &markers[u as usize] {
-            if *node_markers.get(&m).unwrap() == graph.degree_of(u) {
-                naive_nodes.set_bit(u);
-                break;
-            }
-        }
-
-        linear_nodes.set_bit(u);
-    }
-
-    let m = graph.num_edges();
-
-    for u in naive_nodes.iter_set_bits() {
-        graph.remove_edges_at_node(u);
-    }
+    let mut graph = org_graph.clone();
+    let mut timer = Instant::now();
+    let mut domset = DominatingSet::new();
+    let mut rm = Rule1::apply_naive(&mut graph, &mut domset);
+    let mut time = timer.elapsed().as_nanos();
 
     writeln!(
         stderr,
-        "{:20} n -= {:7}, m -= {:7}, |D| += {:7}",
+        "{:20} n -= {:7}, m -= {:7}, |D| += {:7}, Time = {:10}",
         "Rule1-Naive",
-        naive_nodes.cardinality() - 1,
-        m - graph.num_edges() - 1,
-        domset.len()
+        rm.cardinality(),
+        org_graph.num_edges() - graph.num_edges(),
+        domset.len(),
+        time
     )?;
 
-    for u in (linear_nodes.bitmask_stream() - &naive_nodes).iter_set_bits() {
-        graph.remove_edges_at_node(u);
-    }
+    graph = org_graph.clone();
+    timer = Instant::now();
+    domset = DominatingSet::new();
+    rm = Rule1::apply_linear(&mut graph, &mut domset);
+    time = timer.elapsed().as_nanos();
 
     writeln!(
         stderr,
-        "{:20} n -= {:7}, m -= {:7}, |D| += {:7}",
+        "{:20} n -= {:7}, m -= {:7}, |D| += {:7}, Time = {:10}",
         "Rule1-Linear",
-        linear_nodes.cardinality() - 1,
-        m - graph.num_edges() - 1,
-        domset.len()
+        rm.cardinality(),
+        org_graph.num_edges() - graph.num_edges(),
+        domset.len(),
+        time
     )?;
 
-    for u in graph.vertices() {
-        if markers[u as usize].is_empty() {
-            continue;
-        }
-
-        // Rule1-Plus
-        if graph
-            .neighbors_of(u)
-            .filter(|&v| markers[v as usize].is_empty())
-            .count()
-            == 1
-        {
-            linear_nodes.set_bit(u);
-            graph.remove_edges_at_node(u);
-        }
-    }
+    graph = org_graph.clone();
+    timer = Instant::now();
+    domset = DominatingSet::new();
+    rm = Rule1::apply_plus(&mut graph, &mut domset);
+    time = timer.elapsed().as_nanos();
 
     writeln!(
         stderr,
-        "{:20} n -= {:7}, m -= {:7}, |D| += {:7}",
+        "{:20} n -= {:7}, m -= {:7}, |D| += {:7}, Time = {:10}",
         "Rule1-Plus",
-        linear_nodes.cardinality() - 1,
-        m - graph.num_edges() - 1,
-        domset.len()
+        rm.cardinality(),
+        org_graph.num_edges() - graph.num_edges(),
+        domset.len(),
+        time
     )?;
 
-    graph.remove_edges_between(|u| !markers[u as usize].is_empty());
+    graph = org_graph.clone();
+    timer = Instant::now();
+    domset = DominatingSet::new();
+    rm = Rule1::apply_extra(&mut graph, &mut domset);
+    time = timer.elapsed().as_nanos();
 
     writeln!(
         stderr,
-        "{:20} n -= {:7}, m -= {:7}, |D| += {:7}",
+        "{:20} n -= {:7}, m -= {:7}, |D| += {:7}, Time = {:10}",
         "Rule1-Extra",
-        linear_nodes.cardinality() - 1,
-        m - graph.num_edges() - 1,
-        domset.len()
+        rm.cardinality(),
+        org_graph.num_edges() - graph.num_edges(),
+        domset.len(),
+        time
     )?;
 
     // Print reduced graph and domset
     if !args.deny_graph_output {
-        // Add gadgets back in
-        for u in domset.iter() {
-            let nb = org_graph
-                .neighbors_of(u)
-                .find(|&v| linear_nodes.get_bit(v))
-                .unwrap();
-            graph.add_edge(u, nb);
-        }
-
         let mut stdout = std::io::stdout();
         writeln!(
             stdout,
--- src/rule1.rs
+++ src/rule1.rs
@@ -29,7 +29,62 @@ pub struct Rule1;
 const NOT_SET: Node = Node::MAX;
 
 impl Rule1 {
-    pub fn apply_rule(graph: &Graph, sol: &mut DominatingSet) {
+    pub fn apply_naive(graph: &mut Graph, sol: &mut DominatingSet) -> BitSet {
+        let mut marked = BitSet::new(graph.len());
+        let mut type2_nodes = BitSet::new(graph.len());
+
+        let mut redundant = BitSet::new(graph.len());
+        for u in graph.vertices() {
+            if graph.degree_of(u) == 0 && !redundant.get_bit(u) {
+                sol.add_node(u);
+                continue;
+            }
+
+            if redundant.get_bit(u) {
+                continue;
+            }
+
+            marked.clear_all();
+            type2_nodes.clear_all();
+            for v in graph.closed_neighbors_of(u) {
+                marked.set_bit(v);
+            }
+
+            for v in graph.neighbors_of(u) {
+                if graph.closed_neighbors_of(v).all(|x| marked.get_bit(x)) {
+                    if graph.degree_of(v) == graph.degree_of(u) && u < v {
+                        continue;
+                    }
+
+                    type2_nodes.set_bit(v);
+                }
+            }
+
+            let mut type3 = false;
+            for v in graph.neighbors_of(u) {
+                type3 = type3
+                    || graph
+                        .closed_neighbors_of(v)
+                        .all(|x| x == u || type2_nodes.get_bit(x));
+            }
+
+            if type3 {
+                sol.add_node(u);
+                for v in type2_nodes.iter_set_bits() {
+                    graph.remove_edges_at_node(v);
+                    redundant.set_bit(v);
+                }
+
+                // Add gadget
+                let first_bit = type2_nodes.get_first_set_index_atleast(0).unwrap();
+                graph.add_edge(u, first_bit);
+            }
+        }
+
+        redundant
+    }
+
+    pub fn apply_linear(graph: &mut Graph, sol: &mut DominatingSet) -> BitSet {
         let n = graph.len();
 
         // Inverse mappings of step (1) and (2)
@@ -49,6 +104,9 @@ impl Rule1 {
 
         // Helper-BitSet to ensure we only process each node once later
         let mut processed: BitSet = BitSet::new(n);
+
+        // BitSet of removed nodes at the end
+        let mut redundant = BitSet::new(n);
 
         // (1) Compute first mapping and fix possible singletons
         for u in graph.vertices() {
@@ -146,5 +204,347 @@ impl Rule1 {
                 }
             }
         }
+
+        parent = vec![NOT_SET; n as usize];
+        for u in sol.iter() {
+            parent[u as usize] = u;
+            for v in graph.neighbors_of(u) {
+                if parent[v as usize] == NOT_SET {
+                    parent[v as usize] = u;
+                }
+            }
+        }
+
+        for u in graph.vertices() {
+            if parent[u as usize] != u
+                && graph
+                    .closed_neighbors_of(u)
+                    .all(|v| parent[v as usize] != NOT_SET)
+            {
+                redundant.set_bit(u);
+            }
+        }
+
+        for u in redundant.iter_set_bits() {
+            graph.remove_edges_at_node(u);
+        }
+
+        if let Some(first_bit) = redundant.get_first_set_index_atleast(0) {
+            graph.add_edge(parent[first_bit as usize], first_bit);
+        }
+
+        redundant
+    }
+
+    pub fn apply_plus(graph: &mut Graph, sol: &mut DominatingSet) -> BitSet {
+        let n = graph.len();
+
+        // Inverse mappings of step (1) and (2)
+        let mut inv_mappings: Vec<SmallVec<[Node; 4]>> = vec![Default::default(); n as usize];
+
+        // Parent[u] = v if (u,v) is a possible candidate
+        let mut parent: Vec<Node> = vec![NOT_SET; n as usize];
+
+        // Used for confirming whether neighborhoods of nodes are subsets of other neighborhoods.
+        let mut marked: Vec<Node> = vec![NOT_SET; n as usize];
+
+        // List of all possible candidate-pairs
+        let mut potential_type3_node: Vec<(Node, Node)> = Vec::new();
+
+        // BitSet indicating that a node is a Type(2 or 3)-Candidate (not confirmed yet)
+        let mut type2_nodes: BitSet = BitSet::new(n);
+
+        // Helper-BitSet to ensure we only process each node once later
+        let mut processed: BitSet = BitSet::new(n);
+
+        // BitSet of removed nodes at the end
+        let mut redundant = BitSet::new(n);
+
+        // (1) Compute first mapping and fix possible singletons
+        for u in graph.vertices() {
+            let max_neighbor = graph
+                .closed_neighbors_of(u)
+                .map(|u| (graph.degree_of(u), u))
+                .max()
+                .map(|(_, u)| u)
+                .unwrap();
+            if max_neighbor != u {
+                inv_mappings[max_neighbor as usize].push(u);
+            } else if graph.degree_of(u) == 0 {
+                sol.add_node(u);
+            }
+        }
+
+        // (1) Compute list of candidate-pairs based on mapping
+        for u in graph.vertices() {
+            // Mark closed neighborhood N[u] of u (SelfLoop marker)
+            for v in graph.closed_neighbors_of(u) {
+                marked[v as usize] = u;
+            }
+
+            // Check whether N[v] is a subset of N[u]
+            for v in inv_mappings[u as usize].drain(..) {
+                if graph
+                    .closed_neighbors_of(v)
+                    .all(|x| marked[x as usize] == u)
+                {
+                    parent[v as usize] = u;
+                    potential_type3_node.push((v, u));
+
+                    type2_nodes.set_bit(v);
+                }
+            }
+        }
+
+        // We drained inv_mappings earlier completely, so we can now reuse it
+        debug_assert!(inv_mappings.iter().all(|vec| vec.is_empty()));
+
+        // (2) Compute second mapping from list of candidate-pairs
+        for &(u, _) in &potential_type3_node {
+            for v in graph.closed_neighbors_of(u) {
+                // Only process each node once
+                if processed.set_bit(v) {
+                    continue;
+                }
+
+                // Mark closed neighborhood N[v] of v (SelfLoop marker)
+                for x in graph.closed_neighbors_of(v) {
+                    marked[x as usize] = v;
+                }
+
+                // Find minimum dominating node of neighbors in neighborhood of v
+                if let Some((_, min_node)) = graph
+                    .closed_neighbors_of(v)
+                    .filter_map(|x| {
+                        let pt = parent[x as usize];
+                        (pt != NOT_SET && pt != v && marked[pt as usize] == v)
+                            .then(|| (graph.degree_of(pt), pt))
+                    })
+                    .min()
+                {
+                    // We drained inv_mappings earlier completely, so we can now reuse it
+                    inv_mappings[min_node as usize].push(v);
+                }
+            }
+        }
+
+        parent = vec![NOT_SET; n as usize];
+
+        // (3) Mark candidates as possible Type2-Nodes if their neighborhoods are subsets
+        for u in graph.vertices() {
+            // Mark closed neighborhood N[u] of u (SelfLoop marker)
+            for v in graph.closed_neighbors_of(u) {
+                marked[v as usize] = u;
+            }
+
+            for &v in &inv_mappings[u as usize] {
+                if graph
+                    .closed_neighbors_of(v)
+                    .all(|x| marked[x as usize] == u)
+                {
+                    parent[v as usize] = u;
+                }
+            }
+
+            for v in inv_mappings[u as usize].drain(..) {
+                if graph
+                    .closed_neighbors_of(v)
+                    .all(|x| parent[x as usize] == u || x == u)
+                {
+                    sol.add_node(u);
+                    break;
+                }
+            }
+        }
+
+        parent = vec![NOT_SET; n as usize];
+        for u in sol.iter() {
+            parent[u as usize] = u;
+            for v in graph.neighbors_of(u) {
+                if parent[v as usize] == NOT_SET {
+                    parent[v as usize] = u;
+                }
+            }
+        }
+
+        for u in graph.vertices() {
+            if parent[u as usize] != u
+                && parent[u as usize] != NOT_SET
+                && graph
+                    .neighbors_of(u)
+                    .filter(|&v| parent[v as usize] != NOT_SET)
+                    .count()
+                    <= 1
+            {
+                redundant.set_bit(u);
+            }
+        }
+
+        for u in redundant.iter_set_bits() {
+            graph.remove_edges_at_node(u);
+        }
+
+        if let Some(first_bit) = redundant.get_first_set_index_atleast(0) {
+            graph.add_edge(parent[first_bit as usize], first_bit);
+        }
+
+        redundant
+    }
+
+    pub fn apply_extra(graph: &mut Graph, sol: &mut DominatingSet) -> BitSet {
+        let n = graph.len();
+
+        // Inverse mappings of step (1) and (2)
+        let mut inv_mappings: Vec<SmallVec<[Node; 4]>> = vec![Default::default(); n as usize];
+
+        // Parent[u] = v if (u,v) is a possible candidate
+        let mut parent: Vec<Node> = vec![NOT_SET; n as usize];
+
+        // Used for confirming whether neighborhoods of nodes are subsets of other neighborhoods.
+        let mut marked: Vec<Node> = vec![NOT_SET; n as usize];
+
+        // List of all possible candidate-pairs
+        let mut potential_type3_node: Vec<(Node, Node)> = Vec::new();
+
+        // BitSet indicating that a node is a Type(2 or 3)-Candidate (not confirmed yet)
+        let mut type2_nodes: BitSet = BitSet::new(n);
+
+        // Helper-BitSet to ensure we only process each node once later
+        let mut processed: BitSet = BitSet::new(n);
+
+        // BitSet of removed nodes at the end
+        let mut redundant = BitSet::new(n);
+
+        // (1) Compute first mapping and fix possible singletons
+        for u in graph.vertices() {
+            let max_neighbor = graph
+                .closed_neighbors_of(u)
+                .map(|u| (graph.degree_of(u), u))
+                .max()
+                .map(|(_, u)| u)
+                .unwrap();
+            if max_neighbor != u {
+                inv_mappings[max_neighbor as usize].push(u);
+            } else if graph.degree_of(u) == 0 {
+                sol.add_node(u);
+            }
+        }
+
+        // (1) Compute list of candidate-pairs based on mapping
+        for u in graph.vertices() {
+            // Mark closed neighborhood N[u] of u (SelfLoop marker)
+            for v in graph.closed_neighbors_of(u) {
+                marked[v as usize] = u;
+            }
+
+            // Check whether N[v] is a subset of N[u]
+            for v in inv_mappings[u as usize].drain(..) {
+                if graph
+                    .closed_neighbors_of(v)
+                    .all(|x| marked[x as usize] == u)
+                {
+                    parent[v as usize] = u;
+                    potential_type3_node.push((v, u));
+
+                    type2_nodes.set_bit(v);
+                }
+            }
+        }
+
+        // We drained inv_mappings earlier completely, so we can now reuse it
+        debug_assert!(inv_mappings.iter().all(|vec| vec.is_empty()));
+
+        // (2) Compute second mapping from list of candidate-pairs
+        for &(u, _) in &potential_type3_node {
+            for v in graph.closed_neighbors_of(u) {
+                // Only process each node once
+                if processed.set_bit(v) {
+                    continue;
+                }
+
+                // Mark closed neighborhood N[v] of v (SelfLoop marker)
+                for x in graph.closed_neighbors_of(v) {
+                    marked[x as usize] = v;
+                }
+
+                // Find minimum dominating node of neighbors in neighborhood of v
+                if let Some((_, min_node)) = graph
+                    .closed_neighbors_of(v)
+                    .filter_map(|x| {
+                        let pt = parent[x as usize];
+                        (pt != NOT_SET && pt != v && marked[pt as usize] == v)
+                            .then(|| (graph.degree_of(pt), pt))
+                    })
+                    .min()
+                {
+                    // We drained inv_mappings earlier completely, so we can now reuse it
+                    inv_mappings[min_node as usize].push(v);
+                }
+            }
+        }
+
+        parent = vec![NOT_SET; n as usize];
+
+        // (3) Mark candidates as possible Type2-Nodes if their neighborhoods are subsets
+        for u in graph.vertices() {
+            // Mark closed neighborhood N[u] of u (SelfLoop marker)
+            for v in graph.closed_neighbors_of(u) {
+                marked[v as usize] = u;
+            }
+
+            for &v in &inv_mappings[u as usize] {
+                if graph
+                    .closed_neighbors_of(v)
+                    .all(|x| marked[x as usize] == u)
+                {
+                    parent[v as usize] = u;
+                }
+            }
+
+            for v in inv_mappings[u as usize].drain(..) {
+                if graph
+                    .closed_neighbors_of(v)
+                    .all(|x| parent[x as usize] == u || x == u)
+                {
+                    sol.add_node(u);
+                    break;
+                }
+            }
+        }
+
+        parent = vec![NOT_SET; n as usize];
+        for u in sol.iter() {
+            parent[u as usize] = u;
+            for v in graph.neighbors_of(u) {
+                if parent[v as usize] == NOT_SET {
+                    parent[v as usize] = u;
+                }
+            }
+        }
+
+        for u in graph.vertices() {
+            if parent[u as usize] != u
+                && parent[u as usize] != NOT_SET
+                && graph
+                    .neighbors_of(u)
+                    .filter(|&v| parent[v as usize] != NOT_SET)
+                    .count()
+                    <= 1
+            {
+                redundant.set_bit(u);
+            }
+        }
+
+        for u in redundant.iter_set_bits() {
+            graph.remove_edges_at_node(u);
+        }
+
+        graph.remove_edges_between(|u| parent[u as usize] != NOT_SET);
+
+        if let Some(first_bit) = redundant.get_first_set_index_atleast(0) {
+            graph.add_edge(parent[first_bit as usize], first_bit);
+        }
+
+        redundant
     }
 }
